53d6b2abe32ed1ebc015b00f6211fb2e61cc19f3
Orjihenry <orjihen@yahoo.com>
2023-12-15 00:39:08 +0100

    We are all unique! 🌍 🤭 🥳

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,9 +3,9 @@
 -- not fail if DB or TABLE already exists.
 
 CREATE TABLES IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    INDEX ( id )
+    name VARCHAR(255) NOT NULL
+    -- PRIMARY KEY (id),
+    -- INDEX ( id )
 );
