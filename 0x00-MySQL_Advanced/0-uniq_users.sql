307bf8d2dbb364828839ff102d2c021edac3a9f2
Orjihenry <orjihen@yahoo.com>
2023-12-15 00:40:42 +0100

    We are all unique! 🤭 ⭐️ 🥳

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,10 +2,10 @@
 -- IF NOT EXISTS ensures that te script does
 -- not fail if DB or TABLE already exists.
 
-CREATE TABLES IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL
-    -- PRIMARY KEY (id),
-    -- INDEX ( id )
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX ( id )
 );
